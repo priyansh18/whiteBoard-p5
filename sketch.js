0c186e49f2f01cba961d0988e09581a25e30b801
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-05-27 17:10:18 +0530

    Background  Colour Property

--- sketch.js
+++ sketch.js
@@ -8,11 +8,23 @@ function setup() {
   // creating Canvas
   let canvas = createCanvas(1300, 800);
 
-  //Brush colour property
-  brushColor = "#151718";
+  //----------------Brush colour property----------------//
+  brushColor = "#151168";
+  //Getting Element
   document.getElementById("brushColor").value = brushColor;
+  //Implement Event Handler on brush colour
   document.getElementById("brushColor").onchange = function () {
     brushColor = document.getElementById("brushColor").value;
+  };
+
+  //----------------Background colour property-----------//
+  bgColor = "#c8c8c8";
+  // Getting Element
+  document.getElementById("bgColor").value = bgColor;
+  //Implement Event Handler on brush colour
+  document.getElementById("bgColor").onchange = function () {
+    bgColor = document.getElementById("bgColor").value;
+    document.body.style.background = bgColor;
   };
 
   document.getElementsByTagName("canvas")[0].style.cursor = "crosshair";
