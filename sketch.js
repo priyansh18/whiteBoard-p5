17361a5c6a8820d3addc2a2a96558e642a2602de
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-05-27 17:24:52 +0530

    Size Slider Property

--- sketch.js
+++ sketch.js
@@ -1,15 +1,10 @@
-var brushColor;
-var bgColor;
-var drawSize;
-var penStyle;
-var canvas;
 
 function setup() {
   // creating Canvas
   let canvas = createCanvas(1300, 800);
 
   //----------------Brush colour property----------------//
-  brushColor = "#151168";
+   var brushColor = "#151168";
   //Getting Element
   document.getElementById("brushColor").value = brushColor;
   //Implement Event Handler on brush colour
@@ -18,7 +13,7 @@ function setup() {
   };
 
   //----------------Background colour property-----------//
-  bgColor = "#c8c8c8";
+  var bgColor = "#c8c8c8";
   // Getting Element
   document.getElementById("bgColor").value = bgColor;
   //Implement Event Handler on brush colour
@@ -26,6 +21,13 @@ function setup() {
     bgColor = document.getElementById("bgColor").value;
     document.body.style.background = bgColor;
   };
+
+  //----------------Size range property-----------//
+  var drawSize = 4
+  //Implementing Event Handler on Size slider
+  document.getElementById('sizeRange').onchange = function(){
+		var size = map(document.getElementById('sizeRange').value(2,25,4,25));
+		drawSize = size;
 
   document.getElementsByTagName("canvas")[0].style.cursor = "crosshair";
 }
